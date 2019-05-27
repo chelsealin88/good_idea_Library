392438259b9b535e281bfecd59bbe64625d0acef
chelsea <kyuhyun0808@gmail.com>
2019-05-27 14:28:30 +0800

    Initial Commit

---
+++ Good idea Library/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Good idea Library
+//
+//  Created by Chelsea Lin on 2019/5/27.
+//  Copyright © 2019 chelsea lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
